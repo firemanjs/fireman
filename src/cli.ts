45c08ce385bea5b65949c41968b910bb81551f1c
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2018-11-05 18:04:23 +0100

    add firestore command

--- src/cli.ts
+++ src/cli.ts
@@ -17,7 +17,7 @@ const listenForQueries = () => {
   const currentProject = auth.getCurrentProject();
   if (!currentProject || !currentProject.serviceAccountFilename || !currentProject.currentProjectId) {
     rl.question('Enter a service file path: ', input => {
-      auth.addProjectFile(input).then(_ => {
+      auth.addProjectFile(input).then(() => {
         console.log("Project added");
         rl.question('Enter the firestore database url: ', input => {
           auth.addCurrentProjectDb(input);
@@ -69,59 +69,58 @@ const parseQueries = async (input): Promise<void> => {
   }
 };
 
-if (process.argv.length > 2) {
-  commander.version('0.0.1');
-  commander.command("project:use")
-      .description("use another project")
-      .action(async () => {
-        const projects = auth.getAuthenticatedProjects();
-        await inquirer.prompt({
-          type: "list",
-          name: "projectId",
-          message: "Select the project to use",
-          choices: projects.map(project => project.projectId),
-        }).then((ans: any) => {
-          const selectedProject = projects.find(project => project.projectId === ans.projectId);
-          auth.setCurrentProject(selectedProject.projectId, selectedProject.serviceAccountFilename);
-          listenForQueries();
-        }).catch(error => {
-          console.error("Error switching project", error);
-          process.exit(2);
-        });
+commander.version('0.0.1');
+commander.command('firestore')
+    .description('start fireman on firestore')
+    .action(() => listenForQueries());
+commander.command("project:use")
+    .description("use another project")
+    .action(async () => {
+      const projects = auth.getAuthenticatedProjects();
+      await inquirer.prompt({
+        type: "list",
+        name: "projectId",
+        message: "Select the project to use",
+        choices: projects.map(project => project.projectId),
+      }).then((ans: any) => {
+        const selectedProject = projects.find(project => project.projectId === ans.projectId);
+        auth.setCurrentProject(selectedProject.projectId, selectedProject.serviceAccountFilename);
+        listenForQueries();
+      }).catch(error => {
+        console.error("Error switching project", error);
+        process.exit(2);
       });
-  commander.command("project:remove")
-      .description("remove a project")
-      .action(async () => {
-        console.log("REMOVE");
-        const projects = auth.getAuthenticatedProjects();
-        await inquirer.prompt({
-          type: "list",
-          name: "projectId",
-          message: "Select the project to remove",
-          choices: projects.map(project => project.projectId),
-        }).then(async (ans: any) => {
-          const selectedProject = projects.find(project => project.projectId === ans.projectId);
-          await auth.removeProject(selectedProject.projectId);
-          listenForQueries();
-        }).catch(error => {
-          console.error("Error switching project", error);
-          process.exit(2);
-        });
+    });
+commander.command("project:remove")
+    .description("remove a project")
+    .action(async () => {
+      console.log("REMOVE");
+      const projects = auth.getAuthenticatedProjects();
+      await inquirer.prompt({
+        type: "list",
+        name: "projectId",
+        message: "Select the project to remove",
+        choices: projects.map(project => project.projectId),
+      }).then(async (ans: any) => {
+        const selectedProject = projects.find(project => project.projectId === ans.projectId);
+        await auth.removeProject(selectedProject.projectId);
+        listenForQueries();
+      }).catch(error => {
+        console.error("Error switching project", error);
+        process.exit(2);
       });
-  commander.command("project:add <serviceAccountFilePath> <dbUrl>")
-      .description("add project")
-      .action((serviceAccountFilePath, dbUrl) => {
-        auth.addProjectFile(serviceAccountFilePath).then(() => {
-          auth.addCurrentProjectDb(dbUrl);
-          console.log("Project added");
-          listenForQueries();
-        }).catch(error => {
-          console.error("Error adding project", error);
-          process.exit(1);
-        });
+    });
+commander.command("project:add <serviceAccountFilePath> <dbUrl>")
+    .description("add project")
+    .action((serviceAccountFilePath, dbUrl) => {
+      auth.addProjectFile(serviceAccountFilePath).then(() => {
+        auth.addCurrentProjectDb(dbUrl);
+        console.log("Project added");
+        listenForQueries();
+      }).catch(error => {
+        console.error("Error adding project", error);
+        process.exit(1);
       });
-  commander.usage("[command]");
-  commander.parse(process.argv);
-} else {
-  listenForQueries();
-}
+    });
+commander.usage("<command>");
+commander.parse(process.argv);
