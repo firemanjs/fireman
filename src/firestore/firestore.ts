cb4e53a0970f7baf16f17fd328b531116af03d28
Salvatore Giordano <salvatoregiordanoo@gmail.com>
2018-11-23 12:39:57 +0100

    refactoring

--- src/firestore/firestore.ts
+++ src/firestore/firestore.ts
@@ -1,102 +1,42 @@
 import * as FQLParser from '../../parser/parser';
-import {DocumentSnapshot, QueryDocumentSnapshot, QuerySnapshot, CollectionReference} from '@google-cloud/firestore';
+import {
+  CollectionReference,
+  DocumentReference,
+  DocumentSnapshot,
+  Firestore,
+  Query,
+  QueryDocumentSnapshot,
+  QuerySnapshot
+} from '@google-cloud/firestore';
 
 import {Document} from "./document";
 import * as auth from "../auth";
 import * as FirebaseAdmin from "firebase-admin";
 import {QueryResult} from "./query-result";
+import {isArray} from "util";
 
 export let unsubscribeListener;
 
 export type onChangeListener = (result: QueryResult, error: Error) => void;
-
-function checkNotEqualComponent(queries) {
-  let notEqualComponent;
-  queries[0].forEach(component => {
-    if (component.type === ComponentType.COLLECTION_EXPRESSION) {
-      component.components.forEach(c => {
-        if (c.type === 'where' && c.operator === '!=') {
-          notEqualComponent = c;
-        }
-      });
-    }
-  });
-
-  if (notEqualComponent) {
-    queries[1] = queries[0].map(component => {
-      if (component.type === ComponentType.COLLECTION_EXPRESSION) {
-        let modifiedComponent = {...component};
-        modifiedComponent.components = modifiedComponent.components.map(c => {
-          if (c === notEqualComponent) {
-            c = {
-              'type': 'where',
-              'field': c.field,
-              'operator': '<',
-              'value': c.value,
-            }
-          }
-          return c;
-        });
-        return modifiedComponent;
-      }
-
-      return component;
-    });
-
-    queries[0] = queries[0].map(component => {
-      if (component.type === ComponentType.COLLECTION_EXPRESSION) {
-        component.components = component.components.map(c => {
-          if (c === notEqualComponent) {
-            c = {
-              'type': 'where',
-              'field': c.field,
-              'operator': '>',
-              'value': c.value,
-            }
-          }
-          return c;
-        });
-      }
-
-      return component;
-    });
-  }
-}
 
 /**
  * This is useful in case of multiple queries and listeners, to avoid to trigger multiple listeners everytime
  */
 let queryLock = false;
 
-async function executeQueries(initialReference, queries, onChangeListener: onChangeListener, queryString: string) {
-  let result: Document[] = [];
-  let docExpr: boolean = false;
-  await Promise.all(queries.map(async (query) => {
-    const {reference, specificProperties, documentExpression, queryType, recursiveComponents} = parseQuery(initialReference, query);
-    docExpr = documentExpression;
+const executeQuery = async (initialReference, query, onChangeListener: onChangeListener) => {
+  const {references, documentExpression, queryType, nestedQuery} = await parseQuery(initialReference, query);
 
-    if (onChangeListener && recursiveComponents.length > 0) {
-      throw new Error("-l option is not available for nested queries");
-    }
+  if (onChangeListener && nestedQuery) {
+    throw new Error("-l option is not available for nested queries");
+  }
 
-    if (onChangeListener && recursiveComponents.length === 0) {
-      if (queries.length > 1) {
-        if (queries.indexOf(query) === 0) queryLock = false;
-        setListener(queryType, reference, specificProperties, () => {
-          this.query(queryString)
-              .then((result) => onChangeListener(result, null))
-              .catch((e) => onChangeListener(null, e));
-        });
-      } else {
-        setListener(queryType, reference, specificProperties, onChangeListener);
-      }
-    } else {
-      const queryRes = await getResult(queryType, reference, specificProperties, recursiveComponents);
-      result.push(...queryRes);
-    }
-  }));
-  return new QueryResult(result, docExpr);
-}
+  if (onChangeListener) {
+    setListeners(queryType, references, documentExpression, onChangeListener);
+  } else {
+    return new QueryResult(await getResult(queryType, references), documentExpression);
+  }
+};
 
 /**
  * Runs a query against Firebase database
@@ -108,11 +48,9 @@ export const query = async (queryString: string, onChangeListener?: onChangeList
   queryLock = true;
 
   try {
-    let queries = [FQLParser.parse(queryString)];
+    let query = FQLParser.parse(queryString);
 
-    checkNotEqualComponent(queries);
-
-    return await executeQueries(false, queries, onChangeListener, queryString);
+    return await executeQuery(false, query, onChangeListener);
   } catch (e) {
     onChangeListener && onChangeListener(null, e);
     return Promise.reject(e);
@@ -121,30 +59,32 @@ export const query = async (queryString: string, onChangeListener?: onChangeList
   }
 };
 
-function setListener(queryType: QueryType, reference: any, specificProperties: string[], onChangeListener: onChangeListener) {
+function setListeners(queryType: QueryType, references: any[], documentExpression: boolean, onChangeListener: onChangeListener) {
   if (queryType === QueryType.DOCUMENT) {
-    unsubscribeListener = reference.onSnapshot((snapshot: DocumentSnapshot) => {
+    unsubscribeListener = references.map(ref => ref.onSnapshot((snapshot: DocumentSnapshot) => {
       if (snapshot.exists) {
         let document: Document = Document.fromDocumentReference(snapshot.ref);
-        document.setData(snapshot, specificProperties);
-        onChangeListener(new QueryResult([document], specificProperties.length > 0), null);
+        document.setData(snapshot);
+        onChangeListener(new QueryResult([document], documentExpression), null);
       } else {
         onChangeListener(null, Error('No such document'));
       }
-    }, error => onChangeListener(null, error));
+    }, error => onChangeListener(null, error)));
   } else {
-    unsubscribeListener = reference.onSnapshot((snapshot: QuerySnapshot) => {
+    unsubscribeListener = references.map(ref => ref.onSnapshot((snapshot: QuerySnapshot) => {
       let documents: Document[] = [];
       snapshot.forEach((documentSnapshot: QueryDocumentSnapshot) => {
         if (documentSnapshot.exists) {
           let document: Document = Document.fromDocumentReference(documentSnapshot.ref);
-          document.setData(documentSnapshot, specificProperties);
+          document.setData(documentSnapshot);
           documents.push(document);
         }
       });
-      onChangeListener(new QueryResult(documents, specificProperties.length > 0), null);
-    }, error => onChangeListener(null, error));
+      onChangeListener(new QueryResult(documents, documentExpression), null);
+    }, error => onChangeListener(null, error)));
   }
+
+  unsubscribeListener = () => unsubscribeListener.forEach(listener => listener());
 }
 
 enum ComponentType {
@@ -159,119 +99,115 @@ export enum QueryType {
   COLLECTION,
 }
 
-let firebaseAppsInitialized = [];
-let firestore: FirebaseFirestore.Firestore;
+const currentProject = auth.getCurrentProject();
+const serviceAccount = require(`../projects/${currentProject}`);
+FirebaseAdmin.initializeApp({
+  credential: FirebaseAdmin.credential.cert(serviceAccount),
+  databaseURL: serviceAccount.databaseURL,
+}, currentProject);
+const firestore = FirebaseAdmin.firestore(FirebaseAdmin.app(currentProject));
+firestore.settings({timestampsInSnapshots: true});
 
-const parseQuery = (initialReference, components) => {
+const isCollection = (references: any[]) => {
+  return references[0] instanceof CollectionReference || references[0] instanceof Firestore || references[0] instanceof Query;
+};
+
+const parseQuery = async (initialReference, components) => {
+  let references: any[] = initialReference ? [initialReference] : [firestore];
   let documentExpression = false;
-
-  const currentProject = auth.getCurrentProject();
-  if (!firebaseAppsInitialized.includes(currentProject)) {
-    const serviceAccount = require(`../projects/${currentProject}`);
-    FirebaseAdmin.initializeApp({
-      credential: FirebaseAdmin.credential.cert(serviceAccount),
-      databaseURL: serviceAccount.databaseURL,
-    }, currentProject);
-    firebaseAppsInitialized.push(currentProject);
-    firestore = FirebaseAdmin.firestore(FirebaseAdmin.app(currentProject));
-    firestore.settings({timestampsInSnapshots: true});
-  }
-
-  firestore = initialReference || FirebaseAdmin.firestore(FirebaseAdmin.app(currentProject));
-
-  let reference: any = firestore;
-  let collection = true;
-  let recursiveComponents = [];
+  let nestedQuery = false;
   let singleDoc = true;
-  let specificProperties: string[] = [];
-  let br = false;
 
   for (const component of components) {
-    if (br) break;
     switch (component.type) {
       case ComponentType.LITERAL:
         const url = component.value;
-        if (!singleDoc) {
-          recursiveComponents = components.slice(components.indexOf(component));
-          br = true;
-          break;
-        }
-        if (collection) {
-          collection = false;
+        if (isCollection(references)) {
+          if (!singleDoc) {
+            nestedQuery = true;
+            // @ts-ignore
+            references = (await Promise.all<DocumentReference[]>(references.map((ref: CollectionReference) => ref.get()
+                .then(querySnapshot => querySnapshot.docs.map(doc => doc.ref))))).reduce((p, n) => p.concat(n, []));
+          }
 
-          reference = reference.collection(url);
+          references = references.map(ref => ref.collection(url));
         } else {
-          collection = true;
-          reference = reference.doc(url);
+          references = references.map(ref => ref.doc(url));
         }
         break;
       case ComponentType.ALL:
         singleDoc = false;
-        collection = false;
         break;
       case ComponentType.COLLECTION_EXPRESSION:
-        if (reference instanceof CollectionReference) {
-          singleDoc = false;
-          collection = false;
+        singleDoc = false;
 
-          for (const expressionComponent of component.components) {
-            if (expressionComponent.type === 'where') {
-              reference = (<CollectionReference>reference).where(
+        for (const expressionComponent of component.components) {
+          if (expressionComponent.type === 'where') {
+            if (expressionComponent.operator === '!=') {
+              references = [...references.map(ref => ref.where(
+                  expressionComponent.field,
+                  '<',
+                  expressionComponent.value,
+              )),
+                ...references.map(ref => ref.where(
+                    expressionComponent.field,
+                    '>',
+                    expressionComponent.value,
+                ))]
+            } else {
+              references = references.map(ref => ref.where(
                   expressionComponent.field,
                   expressionComponent.operator,
                   expressionComponent.value,
-              );
-            } else if (expressionComponent.type === 'limit') {
-              reference = (<CollectionReference>reference).limit(expressionComponent.limit);
-            } else if (expressionComponent.type === 'order') {
-              reference = (<CollectionReference>reference).orderBy(
-                  expressionComponent.field,
-                  expressionComponent.direction === 1 ? 'asc' : 'desc',
-              );
+              ));
             }
+          } else if (expressionComponent.type === 'limit') {
+            references = references.map(ref => ref.limit(expressionComponent.limit));
+          } else if (expressionComponent.type === 'order') {
+            references = references.map(ref => ref.orderBy(
+                expressionComponent.field,
+                expressionComponent.direction === 1 ? 'asc' : 'desc',
+            ));
           }
         }
         break;
       case ComponentType.DOCUMENT_EXPRESSION:
-        specificProperties = component.components;
+        references = references.map(ref => ref.select(...component.components));
         documentExpression = true;
         break;
     }
   }
 
-  const queryType = !collection ? QueryType.COLLECTION : QueryType.DOCUMENT;
+  const queryType = isArray(references) ? QueryType.COLLECTION : QueryType.DOCUMENT;
   return {
-    reference,
-    specificProperties,
+    references,
     documentExpression,
     queryType,
-    recursiveComponents,
+    nestedQuery,
   };
 };
 
-const getResult = async (queryType, reference: any, specificProperties: string[], recursiveComponents: any[]): Promise<Document[]> => {
+const getResult = async (queryType, references: any[]): Promise<Document[]> => {
   let documents: Document[] = [];
 
   if (queryType === QueryType.DOCUMENT) {
-    const snapshot = await reference.get();
-    if (snapshot.exists) {
-      const document: Document = Document.fromDocumentReference(reference);
-      document.setData(snapshot, specificProperties);
-      documents.push(document);
-    } else {
-      return [];
-    }
-  } else {
-    const snapshot: QuerySnapshot = await reference.get();
-    await Promise.all(snapshot.docs.map(async (docSnapshot: QueryDocumentSnapshot) => {
-      if (recursiveComponents.length > 0) {
-        documents.push(...(await executeQueries(docSnapshot.ref, [recursiveComponents], null, "")).data);
-      } else {
-        const document: Document = Document.fromDocumentReference(docSnapshot.ref);
-        document.setData(docSnapshot, specificProperties);
+    const snapshots = await Promise.all(references.map((reference: DocumentReference) => reference.get()));
+    snapshots.forEach(snapshot => {
+      if (snapshot.exists) {
+        const document: Document = Document.fromDocumentReference(references[0]);
+        document.setData(snapshot);
         documents.push(document);
+      } else {
+        return [];
       }
-    }));
+    });
+  } else {
+    const snapshots: QuerySnapshot[] = await Promise.all(references.map(ref => ref.get()));
+    await Promise.all(snapshots.map(snap => snap.docs.map(async (docSnapshot: QueryDocumentSnapshot) => {
+      const document: Document = Document.fromDocumentReference(docSnapshot.ref);
+      document.setData(docSnapshot);
+      documents.push(document);
+    })));
   }
 
   return documents;
